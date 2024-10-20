f351352e511fd609db3c7e99c6b23c95e11d33a8
Brent-the-carpenter <beh337@gmail.com>
2024-10-19 21:13:49 -0400

    I have got the intial file structor for the project setup. The command tool is working for help and exit command.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Brent-the-carpenter/pokedexcli
+
+go 1.23.2
--- main.go
+++ main.go
@@ -1,8 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+
+	"github.com/Brent-the-carpenter/pokedexcli/cli"
 )
-func main () {
-fmt.Printf("Hello world!")
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := cli.CliCommands()
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			input := scanner.Text()
+
+			if command, ok := commands[input]; ok {
+
+				err := command.Callback(commands)
+				if err != nil {
+					fmt.Println("there was an error: %w", err)
+				}
+				fmt.Println()
+
+			} else {
+				fmt.Println("Unknown command. Type 'help' for a list of commands.")
+			}
+			if input == "exit" {
+				break
+			}
+			fmt.Println()
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("There was an error reading input: %v\n", err)
+			break
+		}
+
+	}
 }
